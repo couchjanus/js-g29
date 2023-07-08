4051ae5c55c3b1acedc12b06f22e53b3b13556e1
couchjanus <couchjanus@gmail.com>
2023-07-08 13:04:01 +0300

    added unit 2

--- js/app.js
+++ js/app.js
@@ -1,101 +1,168 @@
 'use strict';
 
-// alert("Hello from outside script")
 
-/**
- * documatations
- */
 
-// token
+// const totalNumberItemsInWishlist = document.getElementById('total-number-items-in-wishlist')
 
-var a = 1;
+// console.log(totalNumberItemsInWishlist)
 
-// block
-{
-    const b = 1;
-    // b = 5
-    a = 77;
-    let c = a + b;
+// console.log(totalNumberItemsInWishlist.innerText)
+
+// totalNumberItemsInWishlist.innerText = 5
+
+// console.log(totalNumberItemsInWishlist.innerText)
+
+// let addToWishlist = document.querySelector('.add-to-wishlist')
+// console.log(addToWishlist)
+
+
+
+let products = document.querySelectorAll('.product');
+
+// console.log(products[products.length-2]);
+
+console.log(products[6]);
+
+console.log(typeof(products));
+
+console.log(typeof([]));
+
+let arr = [1,2,3,4,5,6,7];
+
+// console.log(arr[arr.length-1]);
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(i);
+// }
+
+// for (let i = 0; i < products.length; i++) {
+//     console.log(products[i]);
+// }
+
+
+// let i = 0;
+
+// while (8 < arr.length) {
+//     console.log(i);
+//     i++;
+// }
+
+// while (i < products.length) {
+//     console.log(products[i]);
+//     i++;
+// }
+
+// do {
+//     console.log(i);
+//     i++;
+// } while (i < arr.length)
+
+
+// for (let i of arr) {
+//     console.log(arr[i]);
+// }
+
+// products.forEach()
+console.log(fnDec())
+
+function fnDec() {
+    return "Declaration fn";
 }
-let b = 2;
-let c = a + b;
-// + - */ %
-let C;
-console.log(typeof(c))
-console.log(typeof('c'))
-console.log(typeof(true))
-console.log(typeof(C))
-console.log(typeof(null))
-
-c++ // c = c + 1
-++c
---c // c = c -1
-c-- 
-
-let f1 = 0.1
-let f2 = 0.2
-
-console.log(f1 + f2)
 
 
-if (f1 + f2 == 0.3) {
-    console.log('0.3')
+// console.log(fnExp())
+
+let fnExp = function() {
+    return 'Function expression';
 }
 
-if (c < 2) {
-    console.log('This is True')
-}
-else if(c == 0) {
-    console.log('This is 0')
-}
-else {
-    console.log('This is False')
-    console.log('c = ', c)
-}
+console.log(fnExp())
 
-
-
-console.log('Hello' + 'String')
-
-console.log(1 + 'String')
-
-console.log('Hello' + 1)
-
-console.log(6/0)
-
-function div(x, y) {
-    if (y == 0) {
-        return "Error ZeroDiv"
-    }
-    return x / y
-}
-
-// console.log(typeof(div))
-
-console.log(div(8, 10))
-
-console.log(document.documentElement)
-
-console.log(document.body)
-// document.all[33].style.backgroundColor = '#aa0033'
-console.log(document.all[33].style.backgroundColor)
-
-// document.body.innerHTML = 'Hello Java Script'
-
-const totalNumberItemsInWishlist = document.getElementById('total-number-items-in-wishlist')
-
-console.log(totalNumberItemsInWishlist)
-
-console.log(totalNumberItemsInWishlist.innerText)
-
-totalNumberItemsInWishlist.innerText = 5
-
-console.log(totalNumberItemsInWishlist.innerText)
-
-let addToWishlist = document.querySelector('.add-to-wishlist')
-console.log(addToWishlist)
 
 function addTest() {
     totalNumberItemsInWishlist.innerText = ++totalNumberItemsInWishlist.innerText
 }
-addToWishlist.addEventListener('click', addTest)
+const totalNumberItemsInWishlist = document.getElementById('total-number-items-in-wishlist')
+let addToWishlist = document.querySelector('.add-to-wishlist')
+
+// addToWishlist.addEventListener('click', addTest)
+
+addToWishlist.addEventListener('click', function() {
+    totalNumberItemsInWishlist.innerText = ++totalNumberItemsInWishlist.innerText
+});
+
+let shopingCartItems = [];
+
+
+products.forEach(function(product) {
+    // console.log(product.querySelector('h3').innerText)
+    // console.log(product.querySelector('.price').innerText)
+
+    product.querySelector('.add-to-cart').addEventListener('click', function() {
+        console.log(product.querySelector('h3').innerText)
+        console.log(product.querySelector('.price').innerText)
+        let shopingCatrItem = {
+            name: '',
+            price: 0
+        }        
+
+        shopingCatrItem.name = product.querySelector('h3').innerText
+        shopingCatrItem.price = +product.querySelector('.price').innerText
+        shopingCartItems.push(shopingCatrItem)
+        console.log(shopingCartItems)
+    })
+    
+})
+
+// function counter() {
+
+//     let value = 0;
+
+//     return function(incrementer) {
+//         value += incrementer;
+//         console.log(value);
+//     }
+// }
+
+// let totalNumber = counter();
+
+// totalNumber(1)
+// totalNumber(10)
+// totalNumber(1)
+// totalNumber(1)
+
+
+function counter() {
+
+    let value = +totalNumberItemsInWishlist.innerText;
+
+    return function(incrementer) {
+        value += incrementer;
+        totalNumberItemsInWishlist.innerText = value
+        console.log(value);
+    }
+}
+
+let totalNumber = counter();
+
+let addToWishlistButtons = document.querySelectorAll('.add-to-wishlist');
+
+addToWishlistButtons.forEach(function(item) {
+    item.addEventListener('click', function() {
+        totalNumber(1);
+    })
+});
+
+function main() {
+
+}
+
+// () => {} function() {return }
+window.addEventListener("DOMContentLoaded", (event) => {
+    console.log('DOM fully loaded and parsed');
+    (() => {
+        console.log(event)
+        main()
+    })();
+
+})
