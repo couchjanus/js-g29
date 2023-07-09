bc2e75372639a7181e77693447258c540e7b0d58
couchjanus <couchjanus@gmail.com>
2023-07-09 13:14:07 +0300

    added unit 3

--- js/app.js
+++ js/app.js
@@ -1,168 +1,216 @@
 'use strict';
 
+class Store {
 
+    static init(key) {
+        if (!Store.isset(key)) {
+            Store.set(key, []);
+        }
+        return Store.get(key);
+    }
 
-// const totalNumberItemsInWishlist = document.getElementById('total-number-items-in-wishlist')
+    static get(key) {
+        let value = localStorage.getItem(key);
+        return value === null ? null : JSON.parse(value);
+    }
 
-// console.log(totalNumberItemsInWishlist)
+    static set(key, value) {
+        localStorage.setItem(key, JSON.stringify(value));
+    }
 
-// console.log(totalNumberItemsInWishlist.innerText)
-
-// totalNumberItemsInWishlist.innerText = 5
-
-// console.log(totalNumberItemsInWishlist.innerText)
-
-// let addToWishlist = document.querySelector('.add-to-wishlist')
-// console.log(addToWishlist)
-
-
-
-let products = document.querySelectorAll('.product');
-
-// console.log(products[products.length-2]);
-
-console.log(products[6]);
-
-console.log(typeof(products));
-
-console.log(typeof([]));
-
-let arr = [1,2,3,4,5,6,7];
-
-// console.log(arr[arr.length-1]);
-
-// for (let i = 0; i < arr.length; i++) {
-//     console.log(i);
-// }
-
-// for (let i = 0; i < products.length; i++) {
-//     console.log(products[i]);
-// }
-
-
-// let i = 0;
-
-// while (8 < arr.length) {
-//     console.log(i);
-//     i++;
-// }
-
-// while (i < products.length) {
-//     console.log(products[i]);
-//     i++;
-// }
-
-// do {
-//     console.log(i);
-//     i++;
-// } while (i < arr.length)
-
-
-// for (let i of arr) {
-//     console.log(arr[i]);
-// }
-
-// products.forEach()
-console.log(fnDec())
-
-function fnDec() {
-    return "Declaration fn";
-}
-
-
-// console.log(fnExp())
-
-let fnExp = function() {
-    return 'Function expression';
-}
-
-console.log(fnExp())
-
-
-function addTest() {
-    totalNumberItemsInWishlist.innerText = ++totalNumberItemsInWishlist.innerText
-}
-const totalNumberItemsInWishlist = document.getElementById('total-number-items-in-wishlist')
-let addToWishlist = document.querySelector('.add-to-wishlist')
-
-// addToWishlist.addEventListener('click', addTest)
-
-addToWishlist.addEventListener('click', function() {
-    totalNumberItemsInWishlist.innerText = ++totalNumberItemsInWishlist.innerText
-});
-
-let shopingCartItems = [];
-
-
-products.forEach(function(product) {
-    // console.log(product.querySelector('h3').innerText)
-    // console.log(product.querySelector('.price').innerText)
-
-    product.querySelector('.add-to-cart').addEventListener('click', function() {
-        console.log(product.querySelector('h3').innerText)
-        console.log(product.querySelector('.price').innerText)
-        let shopingCatrItem = {
-            name: '',
-            price: 0
-        }        
-
-        shopingCatrItem.name = product.querySelector('h3').innerText
-        shopingCatrItem.price = +product.querySelector('.price').innerText
-        shopingCartItems.push(shopingCatrItem)
-        console.log(shopingCartItems)
-    })
-    
-})
-
-// function counter() {
-
-//     let value = 0;
-
-//     return function(incrementer) {
-//         value += incrementer;
-//         console.log(value);
-//     }
-// }
-
-// let totalNumber = counter();
-
-// totalNumber(1)
-// totalNumber(10)
-// totalNumber(1)
-// totalNumber(1)
-
-
-function counter() {
-
-    let value = +totalNumberItemsInWishlist.innerText;
-
-    return function(incrementer) {
-        value += incrementer;
-        totalNumberItemsInWishlist.innerText = value
-        console.log(value);
+    static isset(key) {
+        return this.get(key) !== null;
     }
 }
 
-let totalNumber = counter();
-
-let addToWishlistButtons = document.querySelectorAll('.add-to-wishlist');
-
-addToWishlistButtons.forEach(function(item) {
-    item.addEventListener('click', function() {
-        totalNumber(1);
-    })
-});
-
-function main() {
-
+let cart = [];
+let wishlist = [];
+const modalWindow = document.querySelector('.modal-window');
+function saveWishlist(wishlist) {
+    Store.set('wishlist', wishlist);
+}
+function saveCart(cart) {
+    Store.set('basket', cart);
 }
 
-// () => {} function() {return }
+const productContainer = document.querySelector('.product-container');
+
+let productItemTemplate = (product) => `<div class="product">
+<div class="icons" data-id="${product.id}">
+  <a href="#!" class="fas fa-shopping-cart add-to-cart"></a>
+  <a href="#!" class="fas fa-heart add-to-wishlist"></a>
+  <a href="#productView" class="fas fa-eye detail"></a>
+</div>
+<div class="image">
+  <div class="badge text-white bg-${product.badge.bg}">${product.badge.title}</div>
+  <img src="${product.image}">
+</div>
+
+<div class="content">
+  <h3>${product.name}</h3>
+  <div>$<span class="price">${product.price}</span></div>
+</div>
+</div>`;
+
+
+function populateProductList(products) {
+    let content = '';
+    for (const item of products) {
+        content += productItemTemplate(item);
+    }
+    return content;
+}
+
+let modalTemplate = product => `<div class="modal" id="productView">
+<div class="modal-dialog">
+
+  <div class="modal-content overflow-hidden border-0">
+
+    <a href="#" class="p-4 close btn-close"><i class="fas fa-times"></i></a>
+
+    <div class="modal-body">
+
+      <div class="row align-items-stretch">
+
+        <div class="col-lg-6">
+
+          <div class="bg-center bg-cover d-block h-100"
+            style="background: url(${product.image})"></div>
+
+        </div>
+
+        <div class="col-lg-6">
+
+          <div class="p-4 my-md-4">
+
+            <ul class="list-inline mb-2">
+
+            </ul>
+
+            <h2 class="h4">${product.name}</h2>
+            <p class="text-muted">$${product.price}</p>
+            <p class="text-sm mb-4">${product.description}</p>
+
+            <div class="row align-items-stretch mb-4 btn-block">
+
+              <div class="col-sm-7">
+                <div class="d-flex align-items-center justify-content-between py-1 px-3"><span
+                    class="text-uppercase mr-4">Quantity</span>
+                  <div class="quantity">
+                    <i class="fas fa-caret-left dec-btn"></i>
+                    <input class="form-control" type="text" value="1">
+                    <i class="fas fa-caret-right inc-btn"></i>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-sm-5">
+                <a class="btn btn-primary w-100 d-flex align-items-center justify-content-center" href="#!">Add to cart</a>
+              </div>
+
+            </div>
+
+            <a class="btn btn-link text-dark text-decoration-none" href="#!"><i class="far fa-heart"></i>Add to
+              wish list</a>
+
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+`;
+
+function addProductToWishlist(item = {}) {
+    wishlist = [...wishlist, item];
+    // console.log(wishlist);
+    saveWishlist(wishlist)
+}
+
+function addProductToWishlistButton() {
+    let addToWishlistButtons = document.querySelectorAll('.add-to-wishlist');
+
+    addToWishlistButtons.forEach((item) => {
+        item.addEventListener('click', (event) => {
+            // totalNumber(1);
+            let productId = event.target.closest('.icons').dataset.id;
+            // console.log('Pagent value = ', event.target.closest('.icons').dataset.id)
+            addProductToWishlist({id: productId, name: products[+productId-1].name, price: products[+productId-1].price})
+        })
+    });
+}
+
+function addProductToCart(product, amount=1) {
+
+    let inCart = cart.some(element => element.id === product.id);
+
+    if (inCart) {
+        cart.forEach(item => {
+            if(item.id === product.id) {
+                item.amount += amount;
+            }
+        })
+    } else {
+        let cartItem = {...product, amount:amount};
+        cart = [...cart, cartItem];
+    }
+    
+    saveCart(cart)
+}
+
+function addProductToCartButton() {
+    let addToCartButtons = document.querySelectorAll('.add-to-cart');
+
+    addToCartButtons.forEach((item) => {
+        item.addEventListener('click', (event) => {
+
+            let productId = event.target.closest('.icons').dataset.id;
+            // console.log('Pagent value = ', event.target.closest('.icons').dataset.id)
+            addProductToCart({id: productId, name: products[+productId-1].name, price: products[+productId-1].price})
+        })
+    });
+}
+
+function toggleModal(cart, param, product={}) {
+    if (modalWindow.innerHTML == '') {
+        modalWindow.innerHTML = modalTemplate(product);
+    }else {
+        modalWindow.innerHTML = '';
+    }
+    modalWindow.style.display = param;
+}
+
+function detailButton(products) {
+    let detailButtons = document.querySelectorAll('.detail');
+
+    detailButtons.forEach((item) => {
+        item.addEventListener('click', (event) => {
+
+            let productId = event.target.closest('.icons').dataset.id;
+           
+            // console.log(products)
+            let product = products.find(item => item.id === +productId);
+            console.log(product)
+            toggleModal(cart, 'block', product);
+        })
+    });
+}
+
 window.addEventListener("DOMContentLoaded", (event) => {
-    console.log('DOM fully loaded and parsed');
+    
     (() => {
-        console.log(event)
-        main()
+        cart = Store.init('basket');
+        wishlist = Store.init('wishlist');
+
+        if (productContainer) {
+            productContainer.innerHTML = populateProductList(products);
+            addProductToWishlistButton();
+            addProductToCartButton();
+            detailButton(products);
+        }
     })();
 
 })
---
+++ js/db.js
@@ -0,0 +1,68 @@
+const products = [
+    {
+        id: 1,
+        badge: {
+            title: "Sold",
+            bg: "sold"
+        },
+        image: "https://couchjanus.github.io/images/product-1.jpg",
+        name: "Kui Ye Chen’s AirPods",
+        price: 250,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis.",
+    },
+    {
+        id: 2,
+        badge: {
+            title: "",
+            bg: ""
+        },
+        image: "https://couchjanus.github.io/images/product-2.jpg",
+        name: "Air Jordan 12 gim red",
+        price: 234,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis.",
+    },
+    {
+        id: 3,
+        badge: {
+            title: "New",
+            bg: "new"
+        },
+        image: "https://couchjanus.github.io/images/product-3.jpg",
+        name: "Cyan cotton t-shirt",
+        price: 25,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis.",
+    },
+    {
+        id: 4,
+        badge: {
+            title: "",
+            bg: ""
+        },
+        image: "https://couchjanus.github.io/images/product-4.jpg",
+        name: "Timex Unisex Originals",
+        price: 111,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis.",
+    }, 
+    {
+        id: 5,
+        badge: {
+            title: "Sale",
+            bg: "sale"
+        },
+        image: "https://couchjanus.github.io/images/product-5.jpg",
+        name: "Red didital snartwatch",
+        price: 123,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis.",
+    },  
+    {
+        id: 6,
+        badge: {
+            title: "New",
+            bg: "new"
+        },
+        image: "https://couchjanus.github.io/images/product-6.jpg",
+        name: "Nike air max 25",
+        price: 123,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis.",
+    },  
+];
