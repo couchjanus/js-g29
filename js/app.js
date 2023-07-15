d0c6648a694a20f95daf4bbad4d0df3d9f74f036
couchjanus <couchjanus@gmail.com>
2023-07-15 13:05:35 +0300

    added unit 4

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 'use strict';
-
+// 
 class Store {
 
     static init(key) {
@@ -22,10 +22,11 @@ class Store {
         return this.get(key) !== null;
     }
 }
-
+// 
 let cart = [];
 let wishlist = [];
 const modalWindow = document.querySelector('.modal-window');
+const productsInCart = document.querySelector('.products-in-cart');
 function saveWishlist(wishlist) {
     Store.set('wishlist', wishlist);
 }
@@ -34,6 +35,12 @@ function saveCart(cart) {
 }
 
 const productContainer = document.querySelector('.product-container');
+
+const totalNumberItemsInWishlist = document.querySelector('#total-number-items-in-wishlist');
+
+const totalNumberItemsInCart = document.querySelector('#total-number-items-in-cart');
+
+
 
 let productItemTemplate = (product) => `<div class="product">
 <div class="icons" data-id="${product.id}">
@@ -125,9 +132,13 @@ let modalTemplate = product => `<div class="modal" id="productView">
 `;
 
 function addProductToWishlist(item = {}) {
-    wishlist = [...wishlist, item];
-    // console.log(wishlist);
-    saveWishlist(wishlist)
+    let inWishlist = wishlist.some(e => e.id === item.id);
+
+    if(!inWishlist) {
+        wishlist = [...wishlist, item];
+        saveWishlist(wishlist);
+        wishlistItemsAmount(wishlist)
+    }  
 }
 
 function addProductToWishlistButton() {
@@ -138,7 +149,7 @@ function addProductToWishlistButton() {
             // totalNumber(1);
             let productId = event.target.closest('.icons').dataset.id;
             // console.log('Pagent value = ', event.target.closest('.icons').dataset.id)
-            addProductToWishlist({id: productId, name: products[+productId-1].name, price: products[+productId-1].price})
+            addProductToWishlist({id: productId, price: products[+productId-1].price})
         })
     });
 }
@@ -158,7 +169,8 @@ function addProductToCart(product, amount=1) {
         cart = [...cart, cartItem];
     }
     
-    saveCart(cart)
+    saveCart(cart);
+    cartItemsAmount(cart);
 }
 
 function addProductToCartButton() {
@@ -168,8 +180,8 @@ function addProductToCartButton() {
         item.addEventListener('click', (event) => {
 
             let productId = event.target.closest('.icons').dataset.id;
-            // console.log('Pagent value = ', event.target.closest('.icons').dataset.id)
-            addProductToCart({id: productId, name: products[+productId-1].name, price: products[+productId-1].price})
+            
+            addProductToCart({id: productId, price: products[+productId-1].price})
         })
     });
 }
@@ -199,11 +211,126 @@ function detailButton(products) {
     });
 }
 
+
+const cartItemTemplate = (amount, product) => `<tr class="cart-item" id='id${product.id}'>
+<td class="ps-0 py-3 border-light" scope="row">
+  <a class="reset-anchor d-block" href="detail.html"><img src="${product.image}" alt="${product.name}" width="70"></a>
+</td>
+<td class="ps-0 py-3 border-light" scope="row">
+    <strong><a class="reset-anchor" href="detail.html">${product.name}</a></strong>
+</td>
+<td class="p-3 align-middle border-light">
+  <p class="mb-0 small">$${product.price}</p>
+</td>
+
+<td class="p-3 align-middle border-light">
+  <div class="border d-flex align-items-center justify-content-between px-3"><span class="small text-uppercase text-gray headings-font-family">Quantity</span>
+    <div class="quantity" data-id="${product.id}">
+      <i class="fas fa-caret-left dec-btn"></i>
+      <input class="form-control form-control-sm border-0 shadow-0 p-0" type="text" value="${amount}">
+      <i class="fas fa-caret-right inc-btn"></i>
+    </div>
+  </div>
+</td>
+<td class="p-3 align-middle border-light">
+  <p class="mb-0 small">$<span class="product-subtotal"></span></p>
+</td>
+<td class="p-3 align-middle border-light"><a class="reset-anchor" href="#!"><i class="fas fa-trash-alt small text-muted" data-id="${product.id}"></i></a></td>
+</tr>`;
+
+const findItem = (items, id) => items.find(item => item.id == id);
+
+const populateShoppingCart = (cart, products) => {
+    let result = '';
+    // let id = 3;
+    // console.log(findItem(products, id));
+    cart.forEach(item => result += cartItemTemplate(item.amount, findItem(products, item.id)));
+    return result;
+}
+
+const filterItem = (cart, id) => cart.filter(item => item.id != id);
+
+
+function setCartTotal(cart) {
+    let tmpTotal = 0;
+
+    cart.map(item => {
+        tmpTotal = item.price * item.amount;
+        // console.log(productsInCart.querySelector(`#id${item.id} .product-subtotal`))
+        productsInCart.querySelector(`#id${item.id} .product-subtotal`).innerText = parseFloat(tmpTotal.toFixed(2));
+
+    });
+
+    let subTotal = parseFloat(cart.reduce((prev, cur) => prev + cur.price * cur.amount, 0).toFixed(2));
+
+    let cartTax = subTotal * 0.07;
+
+    document.querySelector('.cart-subtotal').textContent = subTotal;
+    document.querySelector('.cart-tax').textContent = cartTax.toFixed(2);
+    document.querySelector('.cart-total').textContent = subTotal + cartTax;
+
+}
+
+function wishlistItemsAmount(wishlist) {
+    totalNumberItemsInWishlist.textContent = wishlist.length;
+}
+
+function cartItemsAmount(cart) {
+    totalNumberItemsInCart.textContent = cart.reduce((prev, cur) => prev + cur.amount, 0);
+}
+
+function renderCart() {
+    productsInCart.addEventListener('click', event => {
+        if(event.target.classList.contains('fa-trash-alt')) {
+            cart = filterItem(cart, event.target.dataset.id);
+            saveCart(cart);
+            setCartTotal(cart);
+            cartItemsAmount(cart);
+            event.target.closest('.cart-item').remove();
+        }
+        else if (event.target.classList.contains('dec-btn')) {
+            let tmp = findItem(cart, event.target.closest('.quantity').dataset.id);
+
+            if (tmp !== undefined && tmp.amount > 1) {
+                tmp.amount -= 1;
+                event.target.nextElementSibling.value = tmp.amount;
+            } else {
+                cart = filterItem(cart, event.target.closest('.quantity').dataset.id);
+                event.target.closest('.cart-item').remove();
+            }
+            saveCart(cart);
+            setCartTotal(cart);
+            cartItemsAmount(cart);
+        }
+        else if (event.target.classList.contains('inc-btn')) {
+            let tmp = findItem(cart, event.target.closest('.quantity').dataset.id);
+            tmp.amount += 1;
+            event.target.previousElementSibling.value = tmp.amount;
+            saveCart(cart);
+            setCartTotal(cart);
+            cartItemsAmount(cart)
+        }
+    })
+
+
+}
+
 window.addEventListener("DOMContentLoaded", (event) => {
     
     (() => {
         cart = Store.init('basket');
         wishlist = Store.init('wishlist');
+
+        cartItemsAmount(cart)
+        wishlistItemsAmount(wishlist)
+
+        if (productsInCart) {
+            // console.log(productsInCart)
+            productsInCart.innerHTML = populateShoppingCart(cart, products);
+            setCartTotal(cart);
+            renderCart();
+        }
+        
 
         if (productContainer) {
             productContainer.innerHTML = populateProductList(products);
